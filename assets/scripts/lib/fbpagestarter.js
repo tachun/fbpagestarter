0353024e1b282dfc9bef620de70bb95bdd76772a
tachun lin <creed88@gmail.com>
2013-05-31 17:37:48 +0200

    change some js

--- assets/scripts/lib/fbpagestarter.js
+++ assets/scripts/lib/fbpagestarter.js
@@ -1,21 +1,23 @@
-(function( $ ) {
-  var addToPage;
-  
-  $.fn.fbStarter= function(appId, options) {
+(function( $ ) { 
+  $.fn.fbStarter= function(options) {
     var settings = $.extend({
-        appId         : appId,
-        pageId        : '',
-        status        : true, 
-        cookie        : true,
-        xfbml         : true,
-        locales       : 'en_US',
-        activeFangate : true,
-        likedPage     : 'like-wrapper',
-        unlikePage    : 'unlike-wrapper',
-        channelUrl    : ''
+        appId            : '',
+        pageId           : '',
+        status           : true,
+        cookie           : true,
+        xfbml            : true,
+        locales          : 'en_US',
+        activeFangate    : false,
+        likedPage        : 'like-wrapper',
+        unlikePage       : 'unlike-wrapper',
+        canvasUrl        : '',
+        callbackFunction : ''
     }, options);
 
+    var channelUrl = settings.canvasUrl + '/channel.html';
 
+    // FB initiation
+    // -------------------------------------------------------------------
     window.fbInit = window.fbInit || false;
     if (window.fbInit) {
       $(document).trigger('fb:initialized');
@@ -24,7 +26,6 @@
       }
       return;
     }
-
     // Aync Init
     window.fbAsyncInit = function() {
       window.fbInit = true;
@@ -54,9 +55,6 @@
         });
       }
     };
-
-
-
     // Append fb-root
     var fbRoot = 'fb-root';
     if (!document.getElementById(fbRoot)) {
@@ -64,7 +62,6 @@
       element.id = fbRoot;
       document.body.appendChild(element);
     }
-
     // Add Facebook Javascript SDK
     var js, id = 'facebook-jssdk'; 
     if (!document.getElementById(id)) {
@@ -75,95 +72,71 @@
       document.getElementsByTagName('head')[0].appendChild(js);
     }
 
+    // Call a dialog
     // -------------------------------------------------------------------
-    // Install on Page
-    $('.jsAddToPage').on('click', function(e) {
+    $('.fb-dialog').on('click', function(e) {
       e.preventDefault();
-      FB.ui({
-        method: 'pagetab',
-        redirect_uri: 'http:' + $canvas_url + 'install_to_page.html'
-      }, function(response) {
-        if (response != null && response.tabs_added != null) {
-          $.each(response.tabs_added, function(pageid) {
-            FB.api(pageid, function(response) {
-              alert('redirect to ' + response.link);
-            });
-          });
-        } else {
-          alert('Unable to install');
-        }
-      });
-    });
+      $dialogType = $(this).data('fbtype');
 
-    // Post to timeline
-    $('.jsFbShare').on('click', function(e) {
-      e.preventDefault();
-      var obj = {
+      // Install on Page
+      if ($dialogType == 'addtab')
+        console.log($dialogType);
+        FB.ui({
+          method: 'pagetab',
+          redirect_uri: 'http:' + settings.canvasUrl + 'install_to_page.html'
+        }, function(response) {
+          if (response != null && response.tabs_added != null) {
+            $.each(response.tabs_added, function(pageid) {
+              FB.api(pageid, function(response) {
+                alert('redirect to ' + response.link);
+              });
+            });
+          } else {
+            alert('Unable to install');
+          }
+        });
+
+      // Post to timeline
+      if ($dialogType == 'share')
+        console.log($dialogType);
+        var obj = {
           method: 'feed',
           link: $(this).data('link'),
           picture: $(this).data('pic'),
           title: $(this).data('title'),
           caption: $(this).data('caption'),
           description: $(this).data('description')
-      };
-      FB.ui(obj);
+        };
+        FB.ui(obj);
+
+
+      // Post to timeline
+      if ($dialogType == 'permissions')
+        console.log($dialogType);
+        var permissions = $(this).data('permissions');
+
+        FB.login(function(response) {
+          if (response.authResponse) {
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function(response) {
+              console.log('Good to see you, ' + response.name + '.');
+            });
+          } else {
+            console.log('User cancelled login or did not fully authorize.');
+          }
+        }, {scope: permissions});
+
     });
 
-    // Ask Permissions
-    $('.jsAskPermissions').on('click', function(e) {
-      e.preventDefault();
-      var permissions = $(this).data('permissions');
 
-      FB.login(function(response) {
-         if (response.authResponse) {
-           console.log('Welcome!  Fetching your information.... ');
-           FB.api('/me', function(response) {
-             console.log('Good to see you, ' + response.name + '.');
-           });
-         } else {
-           console.log('User cancelled login or did not fully authorize.');
-         }
-       }, {scope: permissions});
+
+    // Callback function
+    // -------------------------------------------------------------------
+    return this.each( function() {
+      if ( $.isFunction( settings.callbackFunction ) ) {
+        settings.callbackFunction.call( this );
+      }
     });
-
-    $(document).on('load', function(e) {
-      console.log('is parent');
-      FB.login(function(response) {
-        console.log('is fb');
-        if (response.session) {
-          var user_id = response.session.uid;
-          var page_id = "109403935810224"; //coca cola
-          var fql_query = "SELECT uid FROM page_fan WHERE page_id = "+page_id+"and uid="+user_id;
-          var the_query = FB.Data.query(fql_query);
-          the_query.wait(function(rows) {
-            if (rows.length == 1 && rows[0].uid == user_id) {
-              console.log('is login');
-              //here you could also do some ajax and get the content for a "liker" instead of simply showing a hidden div in the page.
-            } else {
-              console.log('row=0');
-              //and here you could get the content for a non liker in ajax...
-            }
-          });
-        } else {
-          console.log('not login');
-          // user is not logged in
-        }
-      });
-    });
-
-    //  Set page height
-    function setFbPageHeight(){
-      var heightInterval = setInterval(function(){
-        var newHeight = $('body').outerHeight(true);
-        FB.Canvas.setSize({ width: 810, height: newHeight });
-      }, 1000);
-      setTimeout(function(){
-        clearInterval(heightInterval);
-      },4000);
-    }
-
-
-
     
   };
 })( jQuery );
