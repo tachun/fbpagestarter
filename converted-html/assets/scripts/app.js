a9e2cd60489c7bb39a36b227edfa70a466b351cc
tachun lin <creed88@gmail.com>
2013-06-16 18:00:18 +0200

    Test scrollto

--- converted-html/assets/scripts/app.js
+++ converted-html/assets/scripts/app.js
@@ -13,9 +13,18 @@ $(function(){
     },
     permissionsSuccess : function(){
       FB.api('/me', function(response) {
-        console.log('Good to see you, ' + response.name + '.');
-        $('#user-info').append('<img src="//graph.facebook.com/'+ response.name + '/picture?width=200&height=200">');
+        var userName = "<em>user name:</em> " + response.name,
+            userID = "<em>user id:</em> " + response.id,
+            userBirthday = "<em>user birthday:</em> " + response.birthday,
+            userLocale = "<em>user locale:</em> " + response.locale,
+            userHometown = "<em>user hometown:</em> " + response.hometown.name,
+            userEmail = "<em>user email:</em> " + response.email,
+            userLink = "<em>user link:</em> " + response.link,
+            userPicture = '<img src="//graph.facebook.com/'+response.username+'/picture?width=60&height=60">',
+            userData = userName+'<br>'+userID+'<br>'+userBirthday+'<br>'+userLocale+'<br>'+userHometown+'<br>'+userEmail+'<br>'+userLink+'<br>'+userPicture;
+        $('#user-info').append('<p>Yes! we got permissions!</p>'+userData);
       });
     }
   });
+
 });
